55e9caf4d92777487354f4e366235d69dcd6523b
Ardhi Lukianto <ardhi@lukianto.com>
2023-07-15 11:01:39 +0700

    Updates

--- bajo/init.js
+++ bajo/init.js
@@ -6,12 +6,12 @@ async function connBuilder (c) {
   const { importPkg, error } = this.bajo.helper
   const _ = await importPkg('lodash')
   if (_.isString(c)) c = { url: c }
-  if (!_.has(c, 'path')) throw error('Connection must have path', { code: 'BAJOSP_CONNECTION_PATH_MISSING', connection: c })
+  if (!_.has(c, 'path')) throw error('Connection must have a path', { code: 'BAJOSP_CONNECTION_PATH_MISSING', connection: c })
   if (!_.has(c, 'name')) c.name = c.path
   c.options = c.options || {}
   c.options.parser = c.options.parser || { name: 'ReadlineParser', delimiter: '\r\n' }
-  if (!parsers.includes(c.options.parser.name)) throw error('Unknown parser', { code: 'BAJOSP_PARSER_UNKNOWN', connection: c })
-  if (c.options.decodeNmea && !this.bajoCodec) throw error(`To be able to decode NMEA, you need to load bajoCodec first`, { code: 'BAJOSP_BAJOCODEC_MISSING' })
+  if (!parsers.includes(c.options.parser.name)) throw error(`Unknown parser '%s'`, c.options.parser.name, { code: 'BAJOSP_PARSER_UNKNOWN', connection: c })
+  if (c.options.decodeNmea && !this.bajoCodec) throw error(`To be able to decode NMEA, you need to load 'bajoCodec' first`, { code: 'BAJOSP_BAJOCODEC_MISSING' })
 }
 
 async function prepBroadcasts () {
@@ -22,7 +22,7 @@ async function prepBroadcasts () {
   if (_.isPlainObject(bcs)) bcs = [bcs]
   for (const b of bcs) {
     if (!b.connection) throw error('A broadcast must be bound to a connection', { code: 'BAJOSP_BROADCAST_CONNECTION_MISSING' })
-    if (!_.map(opts.connections, 'name').includes(b.connection)) throw error(`Unknown connection`, { code: 'BAJOSP_BROADCAST_CONNECTION_UNKNOWN' })
+    if (!_.map(opts.connections, 'name').includes(b.connection)) throw error(`Unknown connection  '%s'`, b.connection, { code: 'BAJOSP_BROADCAST_CONNECTION_UNKNOWN' })
     if (!b.receiver) throw error('A broadcast must have destination', { code: 'BAJOSP_BROADCAST_DESTINATION_MISSING' })
     if (!_.isArray(b.receiver)) b.receiver = [b.receiver]
     if (_.isFunction(b.transformer)) b.transformer = await b.transformer.call(this)
