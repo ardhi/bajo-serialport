bc019333e443575e991a91f00a645615b9974f11
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-15 21:19:04 +0700

    Refactor: new error handling

--- bajo/init.js
+++ bajo/init.js
@@ -1,11 +1,10 @@
 async function handler ({ item }) {
-  const { error } = this.app.bajo
   const { isString, has } = this.app.bajo.lib._
-  if (!has(item, 'path')) throw error('Connection must have a path')
+  if (!has(item, 'path')) throw this.error('Connection must have a path')
   item.baudRate = item.baudRate ?? 38400
   if (isString(item.parser)) item.parser = { name: item.parser }
   item.parser = item.parser ?? { name: 'ReadlineParser', delimiter: '\r\n' }
-  if (!this.parsers.includes(item.parser.name)) throw error('Unknown parser \'%s\'', item.parser.name)
+  if (!this.parsers.includes(item.parser.name)) throw this.error('Unknown parser \'%s\'', item.parser.name)
   item.broadcast = item.broadcast ?? false
 }
 
--- bajo/method/send.js
+++ bajo/method/send.js
@@ -1,11 +1,10 @@
 async function send ({ msg, to } = {}) {
-  const { error } = this.app.bajo
   const { find } = this.app.bajo.lib._
   const { addressSplit } = this.app.bajoEmitter
   const { connection, plugin } = addressSplit(to)
   if (plugin !== this.name) return
   const c = find(this.connections, { name: connection })
-  if (!c) throw error('Unknown connection \'%s\'', connection)
+  if (!c) throw this.error('Unknown connection \'%s\'', connection)
   c.instance.write(msg)
 }
 
